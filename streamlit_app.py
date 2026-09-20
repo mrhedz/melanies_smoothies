3c9c990cff53d831dd3b9037514d171709cc6b47
Martín Hernández <eg.sledge94@gmail.com>
2026-09-19 20:03:44 -0600

    Update smoothie order logic and database connection
    Refactor smoothie order submission with new connection method and improved ingredient handling.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,45 +1,46 @@
-# Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-# Write directly to the app
 st.title("🥤 Customize Your Smoothie! 🥤")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be", name_on_order)
 
-session = get_active_session()
+# Connection from Streamlit Community Cloud Secrets
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 my_dataframe = (
     session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS")
     .select(col("FRUIT_NAME"))
 )
 
+fruit_options = [
+    row["FRUIT_NAME"]
+    for row in my_dataframe.collect()
+]
+
 ingredients_list = st.multiselect(
     "Choose Up to 5 Ingredients:",
-    my_dataframe,
+    fruit_options,
     max_selections=5
 )
 
 if ingredients_list:
-
-    ingredients_string = ""
-
-    for fruits_chosen in ingredients_list:
-        ingredients_string += fruits_chosen + " "
-
-    my_insert_stmt = """
-        INSERT INTO SMOOTHIES.PUBLIC.ORDERS
-        (INGREDIENTS, NAME_ON_ORDER)
-        VALUES ('""" + ingredients_string + """', '""" + name_on_order + """')
-    """
+    ingredients_string = " ".join(ingredients_list)
 
     time_to_insert = st.button("Submit Order")
 
     if time_to_insert:
+        my_insert_stmt = f"""
+            INSERT INTO SMOOTHIES.PUBLIC.ORDERS
+            (INGREDIENTS, NAME_ON_ORDER)
+            VALUES ('{ingredients_string}', '{name_on_order}')
+        """
+
         session.sql(my_insert_stmt).collect()
+
         st.success(
             f"Your Smoothie is Ordered, {name_on_order}!",
             icon="✅"
